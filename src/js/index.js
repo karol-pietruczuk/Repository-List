57684b689cad1dcfce9c7b039863a7dabed03c3e
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2020-11-11 21:24:16 +0100

    Functionality and design commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,20 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+fetch('https://api.github.com/users/karol-pietruczuk/repos?sort=created&direction=asc')
+    .then(resp => resp.json())
+    .then(resp => {
+        for(let repo of resp){
+            const {name, html_url} = repo;
+            const repositoryList = document.querySelector('.list--js');
+            const myTemplate = `<li class="list__item">
+            ${name} <a href="${html_url}" title="link do repozytorium ${name} na githubie">
+            link do githuba </a>
+            </li>`;
+            repositoryList.innerHTML += myTemplate;
+        }
+    })
+    .catch(error => {
+        console.log('Nie udało się pobrać');
+    })
